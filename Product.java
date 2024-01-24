e069bfc6723fab8bad2508431348a6b0b0920b7a
Raghavee <78726964+Raghavee15@users.noreply.github.com>
2024-01-24 17:50:44 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,28 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String name;
+    private String description;
+    private double price;
+    private int quantityAvailable;
+
+    // Getters and setters
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantityAvailable=" + quantityAvailable +
+                '}';
+    }
+}
